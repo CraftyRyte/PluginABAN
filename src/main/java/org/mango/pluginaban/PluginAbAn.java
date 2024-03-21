cc94dd24ff7bf8052ce2270ce3a0b3d449f40cdf
CraftyRyte <9210@sanskritischool.edu.in>
2024-03-21 16:56:36 +0530

    Init

---
+++ src/main/java/org/mango/pluginaban/PluginAbAn.java
@@ -0,0 +1,17 @@
+package org.mango.pluginaban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginAbAn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
